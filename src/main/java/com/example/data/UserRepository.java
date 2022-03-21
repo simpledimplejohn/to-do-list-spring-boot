281129a7a0f07fb3420af46e524ae5e717a7e51d
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-21 08:49:04 -0700

    add userrepository

---
+++ src/main/java/com/example/data/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.data;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	public List<User> findByEmailAndPassWord(String email, String password);
+	
+	public List<User> findByEmail(String email);
+
+}
