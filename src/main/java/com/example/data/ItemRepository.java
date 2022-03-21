5601d846ee5cc47b292fb0649c1911d6a7e079e8
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-21 09:11:19 -0700

    add ItemService

--- src/main/java/com/example/data/ItemRepository.java
+++ src/main/java/com/example/data/ItemRepository.java
@@ -1,5 +1,13 @@
 package com.example.data;
 
-public interface ItemRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.model.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer>{
+	List<Item> findAllByToDoList(int to_do_list_id);
 }
---
+++ src/main/java/com/example/service/ItemService.java
@@ -0,0 +1,35 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.data.ItemRepository;
+import com.example.model.Item;
+
+@Service
+public class ItemService {
+	@Autowired
+	ItemRepository itemRepo;
+	
+	public List<Item> findAll() {
+		return itemRepo.findAll();
+	}
+	
+	public Item addItem(Item item) {
+		return itemRepo.save(item);
+	}
+	
+	public List<Item> findItemOfToDo(int id) {
+		return itemRepo.findAllByToDoList(id);
+	}
+	
+	public Item findById(int id) {
+		return itemRepo.getById(id);
+	}
+	
+	public void removeItem(int id) {
+		itemRepo.deleteById(id);
+	}
+}
